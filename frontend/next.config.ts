7108782af3d1f2faad9b93bf7ee74ea618953776
cmdo\41072395860 <wesley.farias@policiamilitar.sp.gov.br>
2025-08-26 14:19:19 -0300

    a

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,13 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-	      ignoreDuringBuilds: true,
-	        },
-		  typescript: {
-			      ignoreBuildErrors: true,
-			        },
-
+ 
+  async rewrites() {
+    return [
+     
+      { source: "/api/chat", destination: "http://127.0.0.1:8081/chat" },
+    ];
+  },
 };
 
 export default nextConfig;
